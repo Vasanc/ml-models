7f5505271be781c08ee115a72be2665bd3f75c10
Vasanc <vasanaids@smvec.ac.in>
2022-10-25 15:30:02 +0530

    completed multivariate regression

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
-linear = pickle.load(open('pkl files\linear.pkl', 'rb'))
+
+
 
 @app.route('/')
 def home():
@@ -13,14 +14,29 @@ def home():
 def linear():
     return render_template('linear.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/multi')
+def multi():
+    return render_template('multi.html')
+
+@app.route('/linearpredicts',methods=['POST'])
 def linearpredict():
+    
+    model = pickle.load(open('pkl files\linear.pkl', 'rb'))
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = linear.predict(final_features)
-
+    prediction = model.predict(final_features)
     output = round(prediction[0], 2)
     return render_template('linear.html', prediction_text='Employee Salary should be ${}'.format(output))
+
+@app.route('/multipredicts',methods=['POST'])
+def multipredict():
+    
+    model = pickle.load(open('pkl files\multi.pkl', 'rb'))
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template('multi.html', prediction_text='Price of the house must be ${}'.format(output))
 
 if __name__ == "__main__":
     app.run()
